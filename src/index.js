4a0fc325731e30a459e82cf5101237715efeafb6
bugszhou <bugszhou@outlook.com>
2019-03-25 23:02:38 +0800

    feat(index.js): add aliapp swan ttapp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  "extends": "stylelint-config-standard",
+  "extends": ["stylelint-config-standard", "stylelint-config-weapp", "stylelint-config-aliapp", "stylelint-config-swan", "stylelint-config-ttapp"],
   "rules": {
     "unit-whitelist": [
       "em",
